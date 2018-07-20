c8870598080af66523a6da8cb9a51adcbdf37fb1
qq576777915 <1@h5.gg>
2018-07-21 00:00:28 +0800

    v2.1 backup

--- qqlevel.php
+++ qqlevel.php
@@ -13,6 +13,7 @@ function king_Regular($result, $re1)
     }
     return $float1;
 }
+
 /**
  * @param $input
  * @param $start
@@ -49,6 +50,8 @@ function king_geturl($url1, $cookie)
 
 $result['code'] = 0;
 $result['qq'] = 'null';
+$result['lock'] = '';
+$result['data'] = '';
 $result['nday'] = 'null';
 $result['svip'] = 'null';  // 0=无会员 1=会员 2=超级会员
 $result['year'] = 'null';  // 是否年费
@@ -61,16 +64,16 @@ $skey = king_geturl("http://www.dkingmz.com/api.php?my=getskey", "");
 if ($qq != null) {
     $result['qq'] = $qq;
     $result['code'] = "";
-    $test = king_geturl("https://vip.qq.com/pk/index?param=" . $qq, "uin=o1393189254; skey=" . $skey);
+    $test = king_geturl("https://vip.qq.com/pk/index?param=" . $qq, "uin=o3478849944; skey=" . $skey);
 
     $GUEST_INFO = get_between($test, "var GUEST_INFO =", "var HOST_LEVEL_INFO =");
-    $GUEST_LEVEL_INFO = get_between($test,"GUEST_LEVEL_INFO"," !window.console && (window.console = {}) && (window.console.log = function () {});");
+    $GUEST_LEVEL_INFO = get_between($test, "GUEST_LEVEL_INFO", " !window.console && (window.console = {}) && (window.console.log = function () {});");
     $result['nday'] = get_between($GUEST_LEVEL_INFO, "iTotalActiveDay\":\"", "\",\"iTotalDays");
     $issVip = get_between($GUEST_INFO, "is_superclub\":", ",\"is_year_club");
     if ($issVip == "1") {
         $result['svip'] = "2";
         $result['level'] = get_between($GUEST_INFO, "level\":", ",\"add_step");
-    } else {
+    } else if ($issVip == "0") {
         $isVip = get_between($GUEST_INFO, "is_club\":", ",\"club_end_timestamp");
         if ($isVip == "1") {
             $result['svip'] = "1";
@@ -80,6 +83,10 @@ if ($qq != null) {
         }
     }
     $result['year'] = get_between($GUEST_INFO, "is_year_club\":", ",\"is_club");
+    if ($result['level'] == null){
+        $result['lock'] = '1';
+        $result['data'] = '前方排队人数过多，小伙伴请稍等会再来哦`';
+    }
     $result['code'] = 1;
     //圈1速度
     //$speed1 = get_between($GUEST_INFO,"qq_speed\":\"","\",\"speed_today");
